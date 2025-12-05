262b6f3a4481623209fcacf4c859156621b3fa12
Heihachic <133248156+Heihachic@users.noreply.github.com>
2025-12-05 13:25:59 +0100

    Update PruebaAutobus.java

--- PruebaAutobus.java
+++ PruebaAutobus.java
@@ -7,7 +7,7 @@ public class PruebaAutobus {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner (System.in);
 		Autobus autobus1 = new Autobus ("1234-ABC", "GRANADA-SEVILLA", 50, );
-
+		System.out.println("Hola este es mi cambio");
 	}
 
 }
